188a6e7aadb1c78d5ece857d7a93c8df7c75ffbb
Miguel Rodenas <migue_rrodenas@example.com>
2025-06-24 21:06:56 -0300

    creamos modelo familiar

--- mi_primer_app/urls.py
+++ mi_primer_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import saludo
+from .views import saludo, saludo_con_template, crear_familiar
 
 urlpatterns = [
     path('hola-mundo/', saludo),
+    path('hola-mundo-template/', saludo_con_template),
+    path('crear-familiar/<str:nombre>/', crear_familiar),
 ]
